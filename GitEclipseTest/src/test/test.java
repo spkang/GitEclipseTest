c6fdead62d9f2ddf6a77c149862aced7e4196f97
spkang <spkangirhit@gmail.com>
2014-04-08 14:21:39 +0800

    add a method

--- GitEclipseTest/src/test/test.java
+++ GitEclipseTest/src/test/test.java
@@ -1,6 +1,9 @@
 package test;
 
 public class test {
+	public void test (String sentence ) {
+		System.out.println ("the input is " + sentence);
+	}
 	public static void main (String [] args) {
 		System.out.println("Hello world!");
 		System.out.println ("Hello GitHub");
