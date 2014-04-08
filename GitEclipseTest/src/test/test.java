a7c419b88e596dce2c119fb74a14ec83a1bc1749
spkang <spkangirhit@gmail.com>
2014-04-08 21:16:52 +0800

    call the method

--- GitEclipseTest/src/test/test.java
+++ GitEclipseTest/src/test/test.java
@@ -1,11 +1,15 @@
 package test;
 
 public class test {
-	public void test (String sentence ) {
+	public void testOne (String sentence ) {
 		System.out.println ("the input is " + sentence);
+		
 	}
+	
 	public static void main (String [] args) {
 		System.out.println("Hello world!");
 		System.out.println ("Hello GitHub");
+		test t = new test();
+		t.testOne("It looks good!");
 	}
 }
