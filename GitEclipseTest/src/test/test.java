1234595bec75f441bd50a5f42a65c70783d16e86
spkang <spkangirhit@gmail.com>
2014-04-09 09:05:44 +0800

    test

--- GitEclipseTest/src/test/test.java
+++ GitEclipseTest/src/test/test.java
@@ -3,13 +3,16 @@ package test;
 public class test {
 	public void testOne (String sentence ) {
 		System.out.println ("the input is " + sentence);
-		
+		sentence = sentence.substring(0, sentence.length() - 2);
+		System.out.println ("the output is " + sentence);
 	}
 	
 	public static void main (String [] args) {
 		System.out.println("Hello world!");
 		System.out.println ("Hello GitHub");
 		test t = new test();
-		t.testOne("It looks good!");
+		String sentence = "It looks good!";
+		t.testOne(sentence);
+		System.out.println("out : " + sentence);
 	}
 }
