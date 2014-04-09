4244a88ebc426bef3ed7e9ea9a17f55219860125
spkang <spkangirhit@gmail.com>
2014-04-09 09:45:40 +0800

    test

--- GitEclipseTest/src/test/test.java
+++ GitEclipseTest/src/test/test.java
@@ -1,5 +1,9 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class test {
 	public void testOne (String sentence ) {
 		System.out.println ("the input is " + sentence);
@@ -7,12 +11,39 @@ public class test {
 		System.out.println ("the output is " + sentence);
 	}
 	
+	public void testTwo (List<String> obj) {
+		if (obj == null )
+			return ;
+		if (!obj.isEmpty() ) {
+			//obj.remove(0);
+			System.out.println("remove 0");
+		}
+		else {
+			obj.add("haha test Two add");
+			obj.add("one");
+		}
+	}
+	
 	public static void main (String [] args) {
-		System.out.println("Hello world!");
-		System.out.println ("Hello GitHub");
-		test t = new test();
-		String sentence = "It looks good!";
-		t.testOne(sentence);
-		System.out.println("out : " + sentence);
+//		System.out.println("Hello world!");
+//		System.out.println ("Hello GitHub");
+//		test t = new test();
+//		String sentence = "It looks good!";
+//		t.testOne(sentence);
+//		System.out.println("out : " + sentence);
+		
+		
+		List<String> objList = new ArrayList<String>();
+		
+		test t  = new test();
+		
+		t.testTwo(objList);
+		
+		System.out.println("list : " + objList.toString());
+		
+		t.testTwo(objList);
+		
+		System.out.println("list : " + objList.toString());
+		
 	}
 }
